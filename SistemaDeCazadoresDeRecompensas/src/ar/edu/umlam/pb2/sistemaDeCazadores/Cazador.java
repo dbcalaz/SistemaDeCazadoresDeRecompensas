1c429adc3618f3795423cf4f3aba9e6890dcfa00
Belén Calaz <dbcalaz@gmail.com>
2025-07-03 17:10:09 -0300

    Se agregó a la clasa Cazador los siguientes métodos: puedeCapturar(), intimidar(), intentarCapturar(), trabajar().

--- SistemaDeCazadoresDeRecompensas/src/ar/edu/umlam/pb2/sistemaDeCazadores/Cazador.java
+++ SistemaDeCazadoresDeRecompensas/src/ar/edu/umlam/pb2/sistemaDeCazadores/Cazador.java
@@ -1,5 +1,6 @@
 package ar.edu.umlam.pb2.sistemaDeCazadores;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Cazador {
@@ -9,6 +10,32 @@ public abstract class Cazador {
 	
 	public Cazador(Integer experiencia) {
 		this.experiencia = experiencia;
+		this.profugosCapturados = new ArrayList<>();
+	}
+	
+	public List<Profugo> getProfugosCapturados() {
+		return profugosCapturados;
 	}
 
+	protected abstract Boolean puedeCapturar(Profugo profugo);
+	
+	protected abstract void intimidar(Profugo profugo);
+	
+	private Boolean intentarCapturar(Profugo profugo) {
+		if(puedeCapturar(profugo)) {
+			this.profugosCapturados.add(profugo);
+			return true;
+		}else {
+			intimidar(profugo);
+		}
+		return false;
+	}
+	
+	public void trabajar(Zona zona) {
+		for(Profugo p : zona.getProfugosEnZona()) {
+			intentarCapturar(p);
+		}
+	}
+	
+	
 }
