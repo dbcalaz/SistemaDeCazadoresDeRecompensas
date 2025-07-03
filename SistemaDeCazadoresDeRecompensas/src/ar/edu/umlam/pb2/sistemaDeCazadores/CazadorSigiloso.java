cba99061360e07cf52f22e9c7fa7e2c05c6563bc
Belén Calaz <dbcalaz@gmail.com>
2025-07-03 17:15:10 -0300

    Se delega la responsabilidad de ser intimidado al prófugo.

--- SistemaDeCazadoresDeRecompensas/src/ar/edu/umlam/pb2/sistemaDeCazadores/CazadorSigiloso.java
+++ SistemaDeCazadoresDeRecompensas/src/ar/edu/umlam/pb2/sistemaDeCazadores/CazadorSigiloso.java
@@ -1,10 +1,25 @@
 package ar.edu.umlam.pb2.sistemaDeCazadores;
 
 public class CazadorSigiloso extends Cazador {
+	
+	private String fraseDeIntimidacion = "Soy un cazador sigiloso";
 
 	public CazadorSigiloso(Integer experiencia) {
 		super(experiencia);
 		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Boolean puedeCapturar(Profugo profugo) {
+		if(this.experiencia > profugo.getNivelDeInocencia() && profugo.getNivelDeHabilidad() < 50	) {
+			return true;
+			}
+		return false;
+	}
+
+	@Override
+	protected void intimidar(Profugo profugo) {
+		profugo.serIntimidado(this.fraseDeIntimidacion);		
 	}
 
 	
